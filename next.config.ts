41beaa967105a81ae388637f5700f38b8e6edc83
Masculinn <hello@burakdev.com>
2025-08-15 23:02:52 +0200

    chore(bundler): next bundle analyzer added

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,29 @@
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
   reactStrictMode: true,
   devIndicators: false,
+  async headers() {
+    return [
+      {
+        source: "/assets/videos/:all*(mp4)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age:31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
