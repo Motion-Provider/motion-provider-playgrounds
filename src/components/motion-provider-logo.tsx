7c36db47728a3ca0f21f795c6fa3558476866ad2
Masculinn <hello@burakdev.com>
2025-09-03 10:35:49 +0200

    feat: pwa optimizations, webmanifest enhancements

--- src/components/motion-provider-logo.tsx
+++ src/components/motion-provider-logo.tsx
@@ -2,7 +2,7 @@ import MotionContainer from "@/motion/motion-container";
 import Image from "next/image";
 
 export const MotionProviderLogo = () => (
-  <div className="absolute md:top-[5.90rem] md:left-30 left-20 top-12 flex items-center justify-center-safe gap-2">
+  <div className="absolute md:top-[6rem] md:left-30 left-20 top-10 flex items-center justify-center-safe gap-2">
     <MotionContainer
       animation={{
         mode: ["fadeIn"],
@@ -13,13 +13,15 @@ export const MotionProviderLogo = () => (
       elementType="div"
       className="blur-3xl size-24 bg-gradient-to-br from-blue-500 via-blue-600 to-40% to-blue-400 absolute -z-20"
     />
-    <div className="flex items-center bg-black rounded-md skew-x-12">
+    <div className="flex items-center rounded-md backdrop-blur-sm">
       <Image
-        src="/motion-provider-logo.png"
+        priority
+        fetchPriority="high"
+        width={240}
+        height={240}
+        src="/motion-provider-logo-transparent.png"
         alt="Motion Provider Logo"
-        width={120}
-        height={120}
-        className="size-11 rounded-md"
+        className="size-10 rounded-md rotate-[13deg]"
       />
     </div>
   </div>
