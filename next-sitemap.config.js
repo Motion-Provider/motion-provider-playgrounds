33f8bac1c744c89b8c116b991b6f1c93eadf4fdc
Masculinn <hello@burakdev.com>
2025-09-03 14:53:28 +0200

    chore: sitemap and seo packages configured

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+﻿/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://motionprovider.dev",
+  generateRobotsTxt: true,
+  autoLastmod: true,
+  outDir: "./out",
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
