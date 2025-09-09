2c79c67854d9e8e3bfa5f603429015e01616da2e
Masculinn <hello@burakdev.com>
2025-09-09 18:56:59 +0200

    refactor: moved outside of motion directory

---
+++ src/utils/getRandomAnimation.ts
@@ -0,0 +1,31 @@
+﻿import {
+  GetRandomAnimation,
+  GetRandomAnimationProps,
+} from "@/interfaces/@types-utils";
+import animations from "@/motion/constants/animations";
+import { AnimationKeys } from "@/motion/constants/animations";
+import logError from "@/motion/utils/getErrorLogs";
+
+export default function getRandomAnimation({
+  count,
+}: GetRandomAnimationProps): GetRandomAnimation {
+  let a: AnimationKeys[] = [];
+
+  if (!count || count <= 0) {
+    logError({
+      msg: "Count should be greater than 0, returning undefined.",
+      src: "getRandomAnimation",
+      mod: "error",
+    });
+    return undefined;
+  }
+
+  for (let i = 0; i < count; i++) {
+    a.push(
+      Object.keys(animations)[
+        Math.floor(Math.random() * Object.keys(animations).length)
+      ] as AnimationKeys
+    );
+  }
+  return a as GetRandomAnimation;
+}
