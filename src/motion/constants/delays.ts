e54aba9d3982b0b9465072a0f3cfb654c43f6ea8
Masculinn <hello@burakdev.com>
2025-09-12 23:49:49 +0200

    feat: delay logic moved to new file for better composition with dynamic functionality, generic typescript types.

---
+++ src/motion/constants/delays.ts
@@ -0,0 +1,28 @@
+﻿const delays = [
+  "linear",
+  "exponential",
+  "sinusoidal",
+  "custom",
+  "square",
+  "triangle",
+  "sawtooth",
+  "cosine",
+  "fibonacci",
+  "pendulum",
+  "perlin",
+  "chaotic",
+  "bounce",
+  "spiral",
+  "quantum",
+  "jitter",
+  "shuffle",
+  "wave",
+  "pingpong",
+  "harmonic",
+  "randomWalk",
+] as const;
+
+export default delays;
+
+type Values<T extends readonly string[]> = T[number];
+export type DelayLogic = Values<typeof delays>;
