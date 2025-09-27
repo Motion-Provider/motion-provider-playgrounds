abc69cac9af9f148ed6e5c0672ebc0b59c65d773
Masculinn <hello@burakdev.com>
2025-09-28 01:29:08 +0200

    feat(analytics): add GA gracefully

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
+import { Analytics } from "@/components/analytics";
 import AppProvider from "@/providers/app-provider";
 import ReduxProvider from "@/providers/redux-provider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ReduxProvider>
       <AppProvider>
+        <Analytics gaId={GA_ID} />
         <Component {...pageProps} />
       </AppProvider>
     </ReduxProvider>
