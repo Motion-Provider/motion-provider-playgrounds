184a9617712a1914aa7f586ea0a29ed833170b62
Masculinn <hello@burakdev.com>
2025-09-28 01:30:47 +0200

    feat: add utility fn in order to check node env

---
+++ src/lib/env.ts
@@ -0,0 +1,18 @@
+﻿export const isServer = typeof window === "undefined";
+
+export function isProd(): boolean {
+  if (process.env.NODE_ENV === "production") return true;
+
+  const runtimeFlag = process.env.NEXT_PUBLIC_RUNTIME_ENV;
+
+  if (runtimeFlag === "production") return true;
+
+  if (!isServer) {
+    const host = window.location.hostname;
+
+    if (host === process.env.NEXT_PUBLIC_SITE_URL! || host !== "localhost")
+      return true;
+  }
+
+  return false;
+}
