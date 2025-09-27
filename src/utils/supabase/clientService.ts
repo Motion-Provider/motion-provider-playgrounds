9280b5edd8e295bf68d407b8820921fca5f3588f
Masculinn <hello@burakdev.com>
2025-09-27 23:07:42 +0200

    hotfix(db): try hoisting env variables

--- src/utils/supabase/clientService.ts
+++ src/utils/supabase/clientService.ts
@@ -1,14 +1,13 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-export default function clientService() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+export default function clientService(): SupabaseClient {
   if (!url || !key) {
-    throw new Error(
-      "Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY env vars"
-    );
+    throw new Error("Missing SUPABASE_URL or SUPABASE_ANON_KEY env vars");
   }
+
   return createClient(url, key, {
     auth: { persistSession: false },
   });
