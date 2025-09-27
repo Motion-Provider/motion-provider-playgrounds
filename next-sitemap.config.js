683c5ff7c0c723da491ae2f7e72c84de3314b064
Masculinn <hello@burakdev.com>
2025-09-28 01:26:09 +0200

    refactor: change site URL logic

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 ﻿/** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://motionprovider.dev",
+  siteUrl: "https://motionprovider.dev",
   generateRobotsTxt: true,
   autoLastmod: true,
   outDir: "./out",
